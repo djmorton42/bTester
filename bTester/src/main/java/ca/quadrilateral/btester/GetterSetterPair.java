f92d691a72e8b40a7289975ed6f4d9b04ab9e5be
Daniel Morton <djmorton@quadrilateral.ca>
2013-02-01 21:49:47 -0500

    UPDATED:  Copyright/License notices to reflect the fact that others are contributing to the project

--- bTester/src/main/java/ca/quadrilateral/btester/GetterSetterPair.java
+++ bTester/src/main/java/ca/quadrilateral/btester/GetterSetterPair.java
@@ -1,7 +1,7 @@
 /*
 bTester License
 
-Copyright 2013 Daniel Morton (djmorton@quadrilateral.ca). All rights reserved.
+Copyright 2013 Daniel Morton (djmorton@quadrilateral.ca) and CONTRIBUTORS. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are
 permitted provided that the following conditions are met:
@@ -13,7 +13,7 @@ permitted provided that the following conditions are met:
       of conditions and the following disclaimer in the documentation and/or other materials
       provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY Daniel Morton ''AS IS'' AND ANY EXPRESS OR IMPLIED
+THIS SOFTWARE IS PROVIDED BY Daniel Morton and CONTRIBUTORS ''AS IS'' AND ANY EXPRESS OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
 FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Daniel Morton OR
 CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
@@ -25,8 +25,8 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 The views and conclusions contained in the software and documentation are those of the
 authors and should not be interpreted as representing official policies, either expressed
-or implied, of Daniel Morton.
-*/
+or implied, of Daniel Morton or contributors.
+ */
 
 package ca.quadrilateral.btester;
 
